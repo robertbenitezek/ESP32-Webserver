1701788010070179275ab3c644959e106b911676
robertbenitezek <robertbenitezek@gmail.com>
2026-04-26 13:53:00 -0700

    added thermistor temperature sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
+#include <math.h>
 #include <WiFi.h>
 #include <FreeRTOS/FreeRTOS.h>
 
 // Replace with your network credentials
 const char* ssid     = "ESP32";
 const char* password = "123456789";
+const int beta = 1;
+const int R1 = 10000; //resistance of R1
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -11,18 +14,34 @@ WiFiServer server(80);
 // Variable to store the HTTP request
 String header;
 
-// Auxiliar variables to store the current output state
+// Display variables to show GPIO current output state
 String output5State = "off";
 String output6State = "off";
 
 // Assign output variables to GPIO pins
 const int output5 = 5;
 const int output6 = 6;
+const int thermistorPin = 1; //1 is ADC1_0
+
+double thermistor_temperature_C(double R, double R0) {
+    const double T0 = 298.15;   // reference temp in Kelvin (25°C)
+    const double B  = 2904.0;   // beta value
+
+    double inv_T = (1.0 / T0) + (1.0 / B) * log(R / R0);
+    double T_K = 1.0 / inv_T;
+
+    double T_C = T_K - 273.15;
+
+    return T_C;
+}
+
 
 void setup() {
 	Serial.begin(115200);
+    analogReadResolution(12); //set analog read resolution to 12 bits (min: 0, max = 2^12 - 1)
+    analogSetAttenuation(ADC_11db); //full 3.3V range for analog readings
 
-	// Initialize the output variables as outputs
+	// Initialize the digital output variables
 	pinMode(output5, OUTPUT);
 	pinMode(output6, OUTPUT);
 
@@ -66,11 +85,18 @@ void loop() {
 						client.println("Connection: close");
 						client.println();
 
+                        int thermistor = analogRead(1); //read voltage of thermistor
+                        double thermistorVoltage = (thermistor*3.3)/4095.0; //convert to voltage
+                        double thermistorResistance = (thermistorVoltage*R1)/(3.3-thermistorVoltage); //convert to resistance
+                        double temperature = thermistor_temperature_C(thermistorResistance, R1);
+                        String displayTemp = String(temperature);
+
 						// turns the GPIOs on and off
 						if (header.indexOf("GET /5/on") >= 0) {
 							Serial.println("GPIO 5 on");
 							output5State = "on";
 							digitalWrite(output5, HIGH);
+
 						} else if (header.indexOf("GET /5/off") >= 0) {
 							Serial.println("GPIO 5 off");
 							output5State = "off";
@@ -98,16 +124,18 @@ void loop() {
 						// Web Page Heading
 						client.println("<body><h1>ESP32 Web Server</h1>");
 
-						// Display current state, and ON/OFF buttons for GPIO 5
-						client.println("<p>GPIO 5 - State " + output5State + "</p>");
+                        // display temperature here:
+                        client.println("<p>Temperature: " + displayTemp + " °C</p>");
 
+						// Display current state, and ON/OFF button for GPIO 5
+						client.println("<p>GPIO 5 - State " + output5State + "</p>");
 						if (output5State == "off") {
 							client.println("<p><a href=\"/5/on\"><button class=\"button\">TURN ON</button></a></p>");
 						} else {
 							client.println("<p><a href=\"/5/off\"><button class=\"button button2\">TURN OFF</button></a></p>");
 						}
 
-						// Display current state, and ON/OFF buttons for GPIO 6
+						// Display current state, and ON/OFF button for GPIO 6
 						client.println("<p>GPIO 6 - State " + output6State + "</p>");
 						if (output6State == "off") {
 							client.println("<p><a href=\"/6/on\"><button class=\"button\">TURN ON</button></a></p>");
